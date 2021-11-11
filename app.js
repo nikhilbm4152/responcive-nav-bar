7412c09d1b820300d607e57c63297ae093943c76
nikhilbm4152 <42427732+nikhilbm4152@users.noreply.github.com>
2021-11-11 12:43:37 +0530

    init

---
+++ app.js
@@ -0,0 +1,34 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav_links");
+  const navLinks = document.querySelectorAll(".nav_links li");
+
+  //   burger togle
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    //animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navlinksFade 0.5s ease forwards ${
+          index / 7 + 1
+        }s`;
+      }
+    });
+
+    // burger cross animation
+    burger.classList.toggle("cross");
+  });
+};
+
+// navSlide()
+
+// if u want to call diffrent function just create an anonymous function and call all the diffrent function in it
+
+const app = () => {
+  navSlide();
+};
+
+app();
